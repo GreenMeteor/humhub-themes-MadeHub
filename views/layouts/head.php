c81235ac2a4ea239b6be1dd4544d2a4513d994bb
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-12-27 05:15:21 -0500

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -2,6 +2,10 @@
 <link href="<?php echo $this->theme->getBaseUrl() . '/fonts/material-icons.css'; ?>" rel="stylesheet">
 <!-- start: CSS -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/styles.css'; ?>" rel="stylesheet">
+<!-- start: scss -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/scss/styles.scss'; ?>" rel="stylesheet">
+<!-- start: less -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/less/styles.less'; ?>" rel="stylesheet">
 <!-- start: JS file imports -->
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js');
 ?>
