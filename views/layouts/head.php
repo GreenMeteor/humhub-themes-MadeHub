c1718693e6dbb055568c9dc81a45ec050b76d2c9
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-12-27 05:18:51 -0500

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -6,6 +6,8 @@
 <link href="<?php echo $this->theme->getBaseUrl() . '/scss/styles.scss'; ?>" rel="stylesheet">
 <!-- start: less -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/less/styles.less'; ?>" rel="stylesheet">
+<!-- start: sass -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/sass/styles.sass'; ?>" rel="stylesheet">
 <!-- start: JS file imports -->
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js');
 ?>
