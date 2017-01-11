40ae16d3be481cadf48cf0c2178b9b216d9eb761
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-11 14:38:36 -0500

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,15 +1,14 @@
 <!-- start: Font -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/fonts/material-icons.css'; ?>" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Philosopher"/>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!-- end: Font -->
 <!-- start: CSS -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/styles.css'; ?>" rel="stylesheet">
-<!-- start: scss -->
-<link href="<?php echo $this->theme->getBaseUrl() . '/scss/styles.scss'; ?>" rel="stylesheet">
-<!-- start: less -->
-<link href="<?php echo $this->theme->getBaseUrl() . '/less/styles.less'; ?>" rel="stylesheet">
-<!-- start: sass -->
-<link href="<?php echo $this->theme->getBaseUrl() . '/sass/styles.sass'; ?>" rel="stylesheet">
-<!-- start: styl -->
-<link href="<?php echo $this->theme->getBaseUrl() . '/styl/styles.styl'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.mCustomScrollbar.min.css'; ?>" rel="stylesheet">
+<!-- end: css -->
 <!-- start: JS file imports -->
-<?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js');
-?>
+<?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js'); ?>
+<!-- end: JS -->
