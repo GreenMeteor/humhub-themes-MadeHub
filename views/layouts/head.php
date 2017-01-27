a5b382f2ba063226f184a4c504229b897ebaab5a
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-27 03:49:15 -0500

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -3,13 +3,11 @@
 <!-- end: Font -->
 
 <!-- start: CSS -->
-<link href="<?php echo $this->theme->getBaseUrl() . '/css/styles.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/flatelements.css'; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.atwho.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.mCustomScrollbar.min.css'; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.atwho.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/waves.css'; ?>" rel="stylesheet">
 
 <!-- end: CSS -->
@@ -17,4 +15,4 @@
 <!-- start: JS file imports -->
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js'); ?>
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/waves.js'); ?>
-<!-- end: JS file imports -->
+<!-- end: JS -->
