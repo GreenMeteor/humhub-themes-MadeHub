205efd97a31ee5a5f8754951c350465360a0bf02
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-27 03:38:15 -0500

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,14 +1,20 @@
 <!-- start: Font -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/fonts/material-icons.css'; ?>" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Philosopher"/>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <!-- end: Font -->
+
 <!-- start: CSS -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/styles.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/flatelements.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.atwho.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.mCustomScrollbar.min.css'; ?>" rel="stylesheet">
-<!-- end: css -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.atwho.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/waves.css'; ?>" rel="stylesheet">
+
+<!-- end: CSS -->
+
 <!-- start: JS file imports -->
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js'); ?>
-<!-- end: JS -->
+<?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/waves.js'); ?>
+<!-- end: JS file imports -->
