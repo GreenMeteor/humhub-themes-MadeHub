be5ead7011a34a3751944824468e863ad1c04874
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-08 16:09:43 -0500

    Update _userProfileLayout.php

--- views/user/account/_userProfileLayout.php
+++ views/user/account/_userProfileLayout.php
@@ -1,18 +1,13 @@
 <?php 
-    humhub\assets\TabbedFormAsset::register($this);
+ humhub\assets\TabbedFormAsset::register($this);
 ?>
 
 <div class="panel-heading">
     <?php echo Yii::t('UserModule.account', '<strong>Your</strong> profile'); ?> <?php echo \humhub\widgets\DataSaved::widget(); ?>
 </div>
 
-<?= humhub\modules\user\widgets\AccountProfilMenu::widget(); ?>
+<?php echo humhub\modules\user\widgets\AccountProfilMenu::widget(); ?>
 
 <div class="panel-body">
     <?php echo $content; ?>
 </div>
-
-
-
-
-
