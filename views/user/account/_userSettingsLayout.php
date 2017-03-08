4596f2c1cdee66c59d46f4c431cd0646b3d2d83f
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-08 16:18:01 -0500

    Update _userSettingsLayout.php

--- views/user/account/_userSettingsLayout.php
+++ views/user/account/_userSettingsLayout.php
@@ -1,18 +1,13 @@
 <?php 
-    humhub\assets\TabbedFormAsset::register($this);
+ humhub\assets\TabbedFormAsset::register($this);
 ?>
 
 <div class="panel-heading">
     <?php echo Yii::t('UserModule.account', '<strong>User</strong> settings'); ?> <?php echo \humhub\widgets\DataSaved::widget(); ?>
 </div>
 
-<?= humhub\modules\user\widgets\AccountSettingsMenu::widget(); ?>
+<?php // humhub\modules\user\widgets\AccountSettingsMenu::widget(); ?>
 
 <div class="panel-body">
     <?php echo $content; ?>
 </div>
-
-
-
-
-
