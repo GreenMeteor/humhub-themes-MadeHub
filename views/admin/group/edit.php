faeacc7500d5ce3fbd3db26d5c532e37cb86d8c2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-08 15:52:37 -0500

    Update edit.php

--- views/admin/group/edit.php
+++ views/admin/group/edit.php
@@ -4,38 +4,38 @@ use yii\widgets\ActiveForm;
 use humhub\compat\CHtml;
 use yii\helpers\Url;
 use yii\helpers\Html;
+use humhub\modules\user\widgets\UserPickerField;
+use humhub\modules\space\widgets\SpacePickerField;
 ?>
 
 <?php $this->beginContent('@admin/views/group/_manageLayout.php', ['group' => $group]) ?>
 <div class="panel-body">
-     <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(); ?>
     <?php echo $form->field($group, 'name'); ?>
     <?php echo $form->field($group, 'description')->textarea(['rows' => 5]); ?>
 
     <?php if (!$group->is_admin_group): ?>
-        <?php echo $form->field($group, 'defaultSpaceGuid')->textInput(['id' => 'space_select']); ?>
         <?php
-        echo \humhub\modules\space\widgets\Picker::widget([
-            'inputId' => 'space_select',
-            'maxSpaces' => 1,
+     echo SpacePickerField::widget([
+            'form' => $form,
             'model' => $group,
-            'attribute' => 'defaultSpaceGuid'
-        ]);
+            'attribute' => 'defaultSpaceGuid',
+            'selection' => $group->defaultSpace,
+            'maxSelection' => 1,
+        ])
         ?>
     <?php endif; ?>
 
     <?php if ($isManagerApprovalSetting && !$group->is_admin_group): ?>
-        <?php echo $form->field($group, 'managerGuids', ['inputOptions' => ['id' => 'user_select']]); ?>
+        <?php $url = ($group->isNewRecord) ? null : Url::to(['/admin/group/admin-user-search', 'id' => $group->id]); ?>
         <?php
-        $url = ($group->isNewRecord) ? null : Url::toRoute('/admin/group/admin-user-search');
-        echo \humhub\modules\user\widgets\UserPicker::widget([
-            'inputId' => 'user_select',
+     echo  UserPickerField::widget([
+            'form' => $form,
             'model' => $group,
             'attribute' => 'managerGuids',
-            'userSearchUrl' => $url,
-            'data' => ['id' => $group->id],
-            'placeholderText' => 'Add a user'
-        ]);
+            'selection' => $group->manager,
+            'url' => $url
+        ])
         ?>
     <?php endif; ?>
 
@@ -47,13 +47,13 @@ use yii\helpers\Html;
     <?php endif; ?>
     <?php echo $form->field($group, 'show_at_directory')->checkbox(); ?>
 
-    <?php echo CHtml::submitButton(Yii::t('AdminModule.views_group_edit', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>  
+    <?php echo Html::submitButton(Yii::t('AdminModule.views_group_edit', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>  
 
     <?php
     if ($showDeleteButton) {
         echo Html::a(Yii::t('AdminModule.views_group_edit', 'Delete'), Url::toRoute(['/admin/group/delete', 'id' => $group->id]), array('class' => 'btn btn-danger', 'data-method' => 'POST'));
     }
     ?>
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 </div>
 <?php $this->endContent(); ?>
