8b90dcba44af9d6a3f8201eb3ff331c9c88b6de5
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-08 15:47:56 -0500

    Update form.php

--- views/post/widgets/form.php
+++ views/post/widgets/form.php
@@ -1,14 +1,8 @@
 <?php
-
-use yii\helpers\Html;
-?>
-
-<?php echo Html::textArea("message", '', array('id' => 'contentForm_message', 'class' => 'form-control autosize contentForm', 'rows' => '1', 'placeholder' => Yii::t("PostModule.widgets_views_postForm", "What's on your mind?"))); ?>
-
-<?php
-
-/* Modify textarea for mention input */
-echo \humhub\widgets\RichTextEditor::widget(array(
-    'id' => 'contentForm_message',
-));
-?>
+echo humhub\widgets\RichtextField::widget([
+   'id' =>  'contentForm_message',
+   'placeholder' => Yii::t("PostModule.widgets_views_postForm", "What's on your mind?"),
+    'name' => 'message',
+    'disabled' => (property_exists(Yii::$app->controller, 'contentContainer') && Yii::$app->controller->contentContainer->isArchived()),
+    'disabledText' => Yii::t("PostModule.widgets_views_postForm", "This space is archived."),
+]);?>
