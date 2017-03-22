fc0a34378bb4a0d1b662afecc032d1eccec98ebe
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-22 14:15:10 -0400

    Update invite.php

--- views/space/create/invite.php
+++ views/space/create/invite.php
@@ -1,5 +1,4 @@
-<?php
-echo \humhub\modules\space\widgets\InviteModal::widget([
+<?= \humhub\modules\space\widgets\InviteModal::widget([
     'model' => $model,
     'submitText' => Yii::t('SpaceModule.views_space_invite', 'Done'),
     'submitAction' => \yii\helpers\Url::to(['/space/create/invite', 'spaceId' => $space->id])
