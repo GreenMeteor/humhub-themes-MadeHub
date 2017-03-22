a0a4ef2e2e0551e2e74108f36c726cfe17849ce6
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-22 16:48:56 -0400

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -5,13 +5,30 @@
 
 <!-- start: CSS -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl() . '/css/flatelements.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/jquery.mCustomScrollbar.min.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/waves.css'; ?>" rel="stylesheet">
-
 <!-- end: CSS -->
+
+<!-- start: Favicon and Touch Icons -->
+<link rel="apple-touch-icon" sizes="57x57" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="<?= $this->theme->getBaseUrl(); ?>/ico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-16x16.png">
+<link rel="manifest" href="<?= $this->theme->getBaseUrl(); ?>/ico/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?= $this->theme->getBaseUrl(); ?>/ico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<!-- end: Favicon and Touch Icons -->
 
 <!-- start: JS file imports -->
 <?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js'); ?>
