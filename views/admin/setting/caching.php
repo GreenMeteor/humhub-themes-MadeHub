d68f56cc037cbc4b06dfd1530dc6274b5c3e750e
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-23 10:09:22 -0400

    Update caching.php

--- views/admin/setting/caching.php
+++ views/admin/setting/caching.php
@@ -1,32 +1,32 @@
 <?php
 
-use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
+use yii\bootstrap\ActiveForm;
+use yii\helper\Html;
 use humhub\models\Setting;
 ?>
 
 <?php $this->beginContent('@admin/views/setting/_advancedLayout.php') ?>
 
-<?php $form = CActiveForm::begin(); ?>
+<?php $form = ActiveForm::begin(); ?>
 
-<?php echo $form->errorSummary($model); ?>
+<?= $form->errorSummary($model); ?>
 
 <div class="form-group">
-    <?php echo $form->labelEx($model, 'type'); ?>
-    <?php echo $form->dropDownList($model, 'type', $cacheTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('cache.class'))); ?>
+    <?= $form->labelEx($model, 'type'); ?>
+    <?= $form->dropDownList($model, 'type', $cacheTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('cache.class'))); ?>
     <br>
 </div>
 
 <div class="form-group">
-    <?php echo $form->labelEx($model, 'expireTime'); ?>
-    <?php echo $form->textField($model, 'expireTime', array('class' => 'form-control', 'readonly' => Setting::IsFixed('cache.expireTime'))); ?>
+    <?= $form->labelEx($model, 'expireTime'); ?>
+    <?= $form->textField($model, 'expireTime', array('class' => 'form-control', 'readonly' => Setting::IsFixed('cache.expireTime'))); ?>
     <br>
 </div>
 
 <hr>
-<?php echo CHtml::submitButton(Yii::t('AdminModule.views_setting_caching', 'Save & Flush Caches'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
+<?= Html::submitButton(Yii::t('AdminModule.views_setting_caching', 'Save & Flush Caches'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
 
-<?php echo \humhub\widgets\DataSaved::widget(); ?>
-<?php CActiveForm::end(); ?>
+<?= \humhub\widgets\DataSaved::widget(); ?>
+<?php ActiveForm::end(); ?>
 
 <?php $this->endContent(); ?>
