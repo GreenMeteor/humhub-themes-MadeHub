13d82d070eeb71a8735b71ce6a59d84abeddfe80
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-30 15:33:14 -0400

    Update mail_plaintext.php

--- views/activity/layouts/mail_plaintext.php
+++ views/activity/layouts/mail_plaintext.php
@@ -1,12 +1,16 @@
 <?php
 
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+ 
 use yii\helpers\Html;
 ?>
 
-<?php echo $content; ?>
+<?= $content; ?>
 
-<?php if (isset($record->content->space) && $record->content->space !== null): ?>(<?php echo strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'via')); ?> <?php echo Html::encode($record->content->space->name); ?>)
+<?php if (isset($record->content->space) && $record->content->space !== null): ?>(<?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'via')); ?> <?= Html::encode($record->content->space->name); ?>)
 
-<?php endif; ?><?php if ($url != "") : ?><?php echo strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'see online')); ?>: <?php echo urldecode($url); ?><?php endif; ?>
-
-
+<?php endif; ?><?php if ($url != "") : ?><?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'See online:')); ?> <?= urldecode($url); ?><?php endif; ?>
