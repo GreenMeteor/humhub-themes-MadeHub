668647fed376f21d02bf8b9b922f83305024aefc
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 17:48:46 -0400

    Update edit.php

--- views/comment/comment/edit.php
+++ views/comment/comment/edit.php
@@ -1,17 +1,17 @@
 <?php
 
-use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 ?>
 
 <div class="content_edit input-container" id="comment_edit_<?= $comment->id; ?>">
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = \yii\widgets\ActiveForm::begin(); ?>
     <?= Html::hiddenInput('contentModel', $contentModel); ?>
     <?= Html::hiddenInput('contentId', $contentId); ?>
   
-    <?= humhub\widgets\RichtextField::widget([
+    <?=
+        humhub\widgets\RichtextField::widget([
             'id' => 'comment_input_' . $comment->id,
             'placeholder' => Yii::t('CommentModule.views_edit', 'Edit your comment...'),
             'model' => $comment,
@@ -20,7 +20,8 @@ use yii\helpers\Url;
 
     <div class="comment-buttons">
 
-        <?= \humhub\modules\file\widgets\UploadButton::widget([
+        <?=
+           \humhub\modules\file\widgets\UploadButton::widget([
             'id' => 'comment_upload_' . $comment->id,
             'model' => $comment,
             'dropZone' => '#comment_' . $comment->id,
@@ -32,22 +33,23 @@ use yii\helpers\Url;
 
         <a href="#" class="btn btn-default btn-sm btn-comment-submit" 
             data-action-click="editSubmit"  
-            data-action-url="<?php echo Url::to(['/comment/comment/edit', 'id' => $comment->id, 'contentModel' => $comment->object_model, 'contentId' => $comment->object_id]) ?>"
+            data-action-url="<?= Url::to(['/comment/comment/edit', 'id' => $comment->id, 'contentModel' => $comment->object_model, 'contentId' => $comment->object_id]) ?>"
             data-action-submit
             data-ui-loader>
-             <?php echo Yii::t('CommentModule.views_edit', 'Save') ?>
+             <?= Yii::t('CommentModule.views_edit', 'Save') ?>
         </a>
 
     </div>
     
     <div id="comment_upload_progress_<?= $comment->id ?>" style="display:none;margin:10px 0px;"></div>
     
-    <?= \humhub\modules\file\widgets\FilePreview::widget([
+    <?=
+       \humhub\modules\file\widgets\FilePreview::widget([
         'id' => 'comment_upload_preview_' . $comment->id,
         'options' => ['style' => 'margin-top:10px'],
         'model' => $comment,
         'edit' => true
     ])?>
 
-    <?php ActiveForm::end(); ?>
+    <?php \yii\widgets\ActiveForm::end(); ?>
 </div>
