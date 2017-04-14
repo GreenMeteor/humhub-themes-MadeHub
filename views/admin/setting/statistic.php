d91de29e1f7efd80f14f8f0c621a8abf873a64ad
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 17:00:34 -0400

    Update statistic.php

--- views/admin/setting/statistic.php
+++ views/admin/setting/statistic.php
@@ -1,7 +1,6 @@
 <?php
 
-use yii;
-use yii\bootstrap\ActiveForm;
+use humhub\compat\CActiveForm;
 use yii\helper\Html;
 use humhub\models\Setting;
 use yii\helpers\Url;
@@ -9,21 +8,21 @@ use yii\helpers\Url;
 <?php $this->beginContent('@admin/views/setting/_advancedLayout.php') ?>
 
 <p><?= Yii::t('AdminModule.views_setting_statistic', 'You can add an statistics HTML code snippet - which will added to all rendered pags.')?></p>
-<br />
+<br>
 
-<?php $form = ActiveForm::begin(); ?>
+<?php $form = CActiveForm::begin(); ?>
 
-<?php echo $form->errorSummary($model); ?>
+<?= $form->errorSummary($model); ?>
 
 <div class="form-group">
     <?= $form->labelEx($model, 'trackingHtmlCode'); ?>
     <?= $form->textArea($model, 'trackingHtmlCode', array('class' => 'form-control', 'rows' => '8')); ?>
 </div>
-<hr>
+<br>
 
 <?= Html::submitButton(Yii::t('AdminModule.views_setting_statistic', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
 
 <?= \humhub\widgets\DataSaved::widget(); ?>
-<?php ActiveForm::end(); ?>
+<?php CActiveForm::end(); ?>
 
 <?php $this->endContent(); ?>
