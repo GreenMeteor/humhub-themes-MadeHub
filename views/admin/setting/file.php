974b89cfb5aecb4ae0c8d428fe4f1de9c61434ba
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 17:06:42 -0400

    Update file.php

--- views/admin/setting/file.php
+++ views/admin/setting/file.php
@@ -1,6 +1,5 @@
 <?php
 
-use humhub\compat\CActiveForm;
 use yii\helper\Html;
 use humhub\models\Setting;
 use yii\helpers\Url;
@@ -9,7 +8,7 @@ use yii\helpers\Url;
 
 
 
-<?php $form = CActiveForm::begin(); ?>
+<?php $form = \yii\widgets\ActiveForm::begin(); ?>
 
 <?= $form->errorSummary($model); ?>
 
@@ -74,6 +73,6 @@ use yii\helpers\Url;
 <?= Html::submitButton(Yii::t('AdminModule.views_setting_file', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
 
 <?= \humhub\widgets\DataSaved::widget(); ?>
-<?php CActiveForm::end(); ?>
+<?php \yii\widgets\ActiveForm::end(); ?>
 
 <?php $this->endContent(); ?>
