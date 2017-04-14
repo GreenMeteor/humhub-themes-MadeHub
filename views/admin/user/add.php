f6ea415aa97746d571eac1df96f1c7b4a5ce9cf4
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 17:03:38 -0400

    Update add.php

--- views/admin/user/add.php
+++ views/admin/user/add.php
@@ -1,4 +1,5 @@
 <?php
+
 use humhub\libs\Html;
 
 humhub\assets\TabbedFormAsset::register($this);
@@ -7,7 +8,7 @@ humhub\assets\TabbedFormAsset::register($this);
 <div class="panel-body">
     <div class="clearfix">
         <?php echo Html::backButton(['index'], ['label' => Yii::t('AdminModule.base', 'Back to overview'), 'class' => 'pull-right']); ?>
-        <h4 class="pull-left"><?php echo Yii::t('AdminModule.views_user_index', 'Add new user'); ?></h4>
+        <h4 class="pull-left"><?= Yii::t('AdminModule.views_user_index', 'Add new user'); ?></h4>
     </div>
     <br />
     <?php $form = \yii\widgets\ActiveForm::begin(['options' => ['data-ui-widget' => 'ui.form.TabbedForm', 'data-ui-init' => '']]); ?>
