4740ec6c0701527673516ce7826b0313c6ac38c1
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 16:53:29 -0400

    Update basic.php

--- views/admin/setting/basic.php
+++ views/admin/setting/basic.php
@@ -1,62 +1,62 @@
 <?php
 
-use humhub\compat\CHtml;
-use yii\widgets\ActiveForm;
+use yii\helper\Html;
+use humhub\compat\CActiveForm;
 
 ?>
 
 <div class="panel-body">
-    <h4><?php echo Yii::t('AdminModule.setting', 'General Settings'); ?></h4>
+    <h4><?= Yii::t('AdminModule.setting', 'General Settings'); ?></h4>
     <div class="help-block">
-        <?php echo Yii::t('AdminModule.setting', 'Here you can configure basic settings of your social network.'); ?>
+        <?= Yii::t('AdminModule.setting', 'Here you can configure basic settings of your social network.'); ?>
     </div>
 
     <br />
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = CActiveForm::begin(); ?>
 
     <?php echo $form->field($model, 'name'); ?>
 
     <?php echo $form->field($model, 'baseUrl'); ?>
-    <p class="help-block"><?php echo Yii::t('AdminModule.views_setting_index', 'E.g. http://example.com/humhub'); ?></p>
+    <p class="help-block"><?= Yii::t('AdminModule.views_setting_index', 'E.g. http://example.com/humhub'); ?></p>
 
     <?php $allowedLanguages = Yii::$app->i18n->getAllowedLanguages(); ?>
     <?php if (count($allowedLanguages) > 1) : ?>
-        <?php echo $languageDropDown = $form->field($model, 'defaultLanguage')->dropDownList($allowedLanguages); ?>
+        <?= $languageDropDown = $form->field($model, 'defaultLanguage')->dropDownList($allowedLanguages); ?>
     <?php endif; ?>
 
-    <?php echo $form->field($model, 'timeZone')->dropDownList(\humhub\libs\TimezoneHelper::generateList()); ?>
+    <?= $form->field($model, 'timeZone')->dropDownList(\humhub\libs\TimezoneHelper::generateList()); ?>
     
-    <?php
-    echo humhub\modules\space\widgets\SpacePickerField::widget([
+    <?=
+    humhub\modules\space\widgets\SpacePickerField::widget([
         'form' => $form,
         'model' => $model,
         'attribute' => 'defaultSpaceGuid',
         'selection' => $model->defaultSpaces
     ])?>
     
-    <?php echo $form->field($model, 'defaultStreamSort')->dropDownList($model->getDefaultStreamSortOptions()); ?>
+    <?= $form->field($model, 'defaultStreamSort')->dropDownList($model->getDefaultStreamSortOptions()); ?>
 
     <p class="help-block"><?php echo Yii::t('AdminModule.views_setting_index', 'New users will automatically be added to these space(s).'); ?></p>
 
 
-    <strong><?php echo Yii::t('AdminModule.views_setting_index', 'Dashboard'); ?></strong>
+    <strong><?= Yii::t('AdminModule.views_setting_index', 'Dashboard'); ?></strong>
     <br>
     <br>
-    <?php echo $form->field($model, 'tour')->checkbox(); ?>
-    <?php echo $form->field($model, 'dashboardShowProfilePostForm')->checkbox(); ?>
+    <?= $form->field($model, 'tour')->checkbox(); ?>
+    <?= $form->field($model, 'dashboardShowProfilePostForm')->checkbox(); ?>
 
-    <strong><?php echo Yii::t('AdminModule.views_setting_index', 'Friendship'); ?></strong>
+    <strong><?= Yii::t('AdminModule.views_setting_index', 'Friendship'); ?></strong>
     <br>
     <br>
-    <?php echo $form->field($model, 'enableFriendshipModule')->checkbox(); ?>
+    <?= $form->field($model, 'enableFriendshipModule')->checkbox(); ?>
 
     <hr>
 
-    <?php echo CHtml::submitButton(Yii::t('AdminModule.views_setting_index', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
+    <?= Html::submitButton(Yii::t('AdminModule.views_setting_index', 'Save'), array('class' => 'btn btn-primary', 'data-ui-loader' => "")); ?>
 
     <!-- show flash message after saving -->
     <?php \humhub\widgets\DataSaved::widget(); ?>
 
-    <?php ActiveForm::end(); ?>
+    <?php CActiveForm::end(); ?>
 </div>
