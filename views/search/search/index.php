9d43e3b23816897f5d202f5541df17c6b71b2b97
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-14 17:33:53 -0400

    Update index.php

--- views/search/search/index.php
+++ views/search/search/index.php
@@ -1,16 +1,19 @@
 <?php
+
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use humhub\modules\search\models\forms\SearchForm;
 use humhub\modules\content\components\ContentActiveRecord;
 use humhub\modules\content\components\ContentContainerActiveRecord;
+
+humhub\modules\stream\assets\StreamAsset::register($this);
 ?>
-<div class="container">
+<div class="container" data-action-component="stream.SimpleStream">
     <div class="row">
         <div class="col-md-12">
             <div class="panel">
-                <div class="panel-heading"><strong><?php echo Yii::t('base', 'Search'); ?></strong></div>
+                <div class="panel-heading"><strong><?= Yii::t('base', 'Search'); ?></strong></div>
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-3"></div>
@@ -18,21 +21,20 @@ use humhub\modules\content\components\ContentContainerActiveRecord;
                             <?php $form = ActiveForm::begin(['action' => Url::to(['index']), 'method' => 'GET']); ?>
                                 <div class="form-group form-group-search">
                                     <?= $form->field($model, 'keyword')->textInput(['placeholder' => Yii::t('SearchModule.views_search_index', 'Search for user, spaces and content'), 'class' => 'form-control form-search', 'id' => 'search-input-field'])->label(false); ?>
-                                    <?php echo Html::submitButton(Yii::t('base', 'Search'), ['class' => 'btn btn-default btn-sm form-button-search', 'data-ui-loader' => '']); ?>
+                                    <?= Html::submitButton(Yii::t('base', 'Search'), ['class' => 'btn btn-default btn-sm form-button-search', 'data-ui-loader' => '']); ?>
                                 </div>
 
                                 <div class="text-center">
                                     <a data-toggle="collapse" id="search-settings-link" href="#collapse-search-settings"
                                        style="font-size: 11px;"><i
-                                            class="fa fa-caret-right"></i> <?php echo Yii::t('SearchModule.views_search_index', 'Advanced search settings'); ?>
+                                            class="fa fa-caret-right"></i> <?= Yii::t('SearchModule.views_search_index', 'Advanced search settings'); ?>
                                     </a>
                                 </div>
 
                                 <div id="collapse-search-settings" class="panel-collapse collapse">
                                     <br>
-                                    <?php echo  Yii::t('SearchModule.views_search_index', 'Search only in certain spaces:'); ?>
-                                    <?php
-                    echo \humhub\modules\space\widgets\SpacePickerField::widget([
+                                    <?=  Yii::t('SearchModule.views_search_index', 'Search only in certain spaces:'); ?>
+                                    <?= \humhub\modules\space\widgets\SpacePickerField::widget([
                                         'id' => 'space_filter',
                                         'model' => $model,
                                         'attribute' => 'limitSpaceGuids',
@@ -55,41 +57,41 @@ use humhub\modules\content\components\ContentContainerActiveRecord;
             <div class="col-md-2">
                 <div class="panel panel-default">
                     <div
-                        class="panel-heading"><?php echo Yii::t('SearchModule.views_search_index', '<strong>Search </strong> results'); ?></div>
+                        class="panel-heading"><?= Yii::t('SearchModule.views_search_index', '<strong>Search </strong> results'); ?></div>
                     <div class="list-group">
-                        <a data-pjax-prevent href='<?php echo Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_ALL]); ?>'
+                        <a data-pjax-prevent href='<?= Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_ALL]); ?>'
                            class="list-group-item <?php if ($model->scope == SearchForm::SCOPE_ALL): ?>active<?php endif; ?>">
                             <div>
-                                <div class="edit_group "><?php echo Yii::t('SearchModule.views_search_index', 'All'); ?>
-                                    (<?php echo $totals[SearchForm::SCOPE_ALL]; ?>)
+                                <div class="edit_group "><?= Yii::t('SearchModule.views_search_index', 'All'); ?>
+                                    (<?= $totals[SearchForm::SCOPE_ALL]; ?>)
                                 </div>
                             </div>
                         </a>
                         <br/>
-                        <a data-pjax-prevent href='<?php echo Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_CONTENT]); ?>'
+                        <a data-pjax-prevent href='<?= Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_CONTENT]); ?>'
                            class="list-group-item <?php if ($model->scope == SearchForm::SCOPE_CONTENT): ?>active<?php endif; ?>">
                             <div>
                                 <div
-                                    class="edit_group "><?php echo Yii::t('SearchModule.views_search_index', 'Content'); ?>
-                                    (<?php echo $totals[SearchForm::SCOPE_CONTENT]; ?>)
+                                    class="edit_group "><?= Yii::t('SearchModule.views_search_index', 'Content'); ?>
+                                    (<?= $totals[SearchForm::SCOPE_CONTENT]; ?>)
                                 </div>
                             </div>
                         </a>
-                        <a data-pjax-prevent href='<?php echo Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_USER]); ?>'
+                        <a data-pjax-prevent href='<?= Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_USER]); ?>'
                            class="list-group-item <?php if ($model->scope == SearchForm::SCOPE_USER): ?>active<?php endif; ?>">
                             <div>
                                 <div
-                                    class="edit_group "><?php echo Yii::t('SearchModule.views_search_index', 'Users'); ?>
-                                    (<?php echo $totals[SearchForm::SCOPE_USER]; ?>)
+                                    class="edit_group "><?= Yii::t('SearchModule.views_search_index', 'Users'); ?>
+                                    (<?= $totals[SearchForm::SCOPE_USER]; ?>)
                                 </div>
                             </div>
                         </a>
-                        <a data-pjax-prevent href='<?php echo Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_SPACE]); ?>'
+                        <a data-pjax-prevent href='<?= Url::to(['/search/search/index', 'SearchForm[keyword]' => $model->keyword, 'SearchForm[limitSpaceGuids]' => $model->limitSpaceGuids, 'SearchForm[scope]' => SearchForm::SCOPE_SPACE]); ?>'
                            class="list-group-item <?php if ($model->scope == SearchForm::SCOPE_SPACE): ?>active<?php endif; ?>">
                             <div>
                                 <div
-                                    class="edit_group "><?php echo Yii::t('SearchModule.views_search_index', 'Spaces'); ?>
-                                    (<?php echo $totals[SearchForm::SCOPE_SPACE]; ?>)
+                                    class="edit_group "><?= Yii::t('SearchModule.views_search_index', 'Spaces'); ?>
+                                    (<?= $totals[SearchForm::SCOPE_SPACE]; ?>)
                                 </div>
                             </div>
                         </a>
@@ -103,32 +105,27 @@ use humhub\modules\content\components\ContentContainerActiveRecord;
 
                     <?php if (count($results) > 0): ?>
                         <?php foreach ($results as $result): ?>
-
-                            <?php if ($result instanceof ContentActiveRecord || $result instanceof ContentContainerActiveRecord) : ?>
-                                <?php echo $result->getWallOut(); ?>
-                            <?php else {
-    : ?>
-                                No Output for Class <?php echo get_class($result);
-}
-?>
+                            <?php if ($result instanceof ContentActiveRecord) : ?>
+                                <?= humhub\modules\stream\actions\Stream::renderEntry($result) ?>
+                            <?php elseif ($result instanceof ContentContainerActiveRecord) : ?>
+                                 <?= $result->getWallOut(); ?>
+                            <?php else: ?>
+                                No Output for Class <?= get_class($result); ?>
                             <?php endif; ?>
                         <?php endforeach; ?>
-                    <?php else {
-    : ?>
+                    <?php else: ?>
 
 
                         <div class="panel panel-default">
                             <div class="panel-body">
-                                <p><strong><?php echo Yii::t('SearchModule.views_search_index', 'Your search returned no matches.');
-}
-?></strong></p>
+                                <p><strong><?= Yii::t('SearchModule.views_search_index', 'Your search returned no matches.'); ?></strong></p>
                             </div>
                         </div>
                     <?php endif; ?>
                 </div>
 
                 <div
-                    class="pagination-container"><?php echo humhub\widgets\LinkPager::widget(['pagination' => $pagination]); ?></div>
+                    class="pagination-container"><?= humhub\widgets\LinkPager::widget(['pagination' => $pagination]); ?></div>
                 <br><br>
             </div>
 
@@ -156,9 +153,9 @@ use humhub\modules\content\components\ContentContainerActiveRecord;
         $('#search-settings-link i').addClass('fa-caret-right');
     });
 <?php foreach (explode(" ", $model->keyword) as $k) : ?>
-        $(".searchResults").highlight("<?php echo Html::encode($k); ?>");
+        $(".searchResults").highlight("<?= Html::encode($k); ?>");
         $(document).ajaxComplete(function (event, xhr, settings) {
-            $(".searchResults").highlight("<?php echo Html::encode($k); ?>");
+            $(".searchResults").highlight("<?= Html::encode($k); ?>");
         });
 <?php endforeach; ?>
 </script>
