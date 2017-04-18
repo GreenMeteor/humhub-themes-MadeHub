22a7a08aaf5133462652a049c0b56126c01b3326
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-18 04:16:16 -0400

    Update editLink.php

--- views/content/widgets/editLink.php
+++ views/content/widgets/editLink.php
@@ -1,9 +1,10 @@
 <?php
 
-use humhub\modules\content\widgets\Wall
-Entry;
+use humhub\modules\content\widgets\WallEntry;
+
 /* @var $this humhub\components\View */
 ?>
+
 <li>
     <?php if($mode === WallEntry::EDIT_MODE_INLINE) : ?>
             <a href="#" class="stream-entry-edit-link" data-action-click="edit" data-action-url="<?= $editUrl ?>">
@@ -14,11 +15,11 @@ Entry;
             </a>
     <?php elseif ($mode === WallEntry::EDIT_MODE_MODAL) : ?>
             <a href="#" class="stream-entry-edit-link" data-action-click="editModal" data-action-url="<?= $editUrl ?>">
-                <i class="fa fa-pencil"></i><?= Yii::t('ContentModule.widgets_views_editLink', 'Edit') ?>
+                <i class="fa fa-pencil"></i><?=  Yii::t('ContentModule.widgets_views_editLink', 'Edit') ?>
             </a>
     <?php elseif ($mode === WallEntry::EDIT_MODE_NEW_WINDOW) : ?>
             <a href="<?= $editUrl ?>" class="stream-entry-edit-link">
-                <i class="fa fa-pencil"></i><?= Yii::t('ContentModule.widgets_views_editLink', 'Edit') ?>
+                <i class="fa fa-pencil"></i><?=  Yii::t('ContentModule.widgets_views_editLink', 'Edit') ?>
             </a>
     <?php endif; ?>
 </li>
