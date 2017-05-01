b096af28e45342749757a6c2ea272b88660ce8bf
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-05-01 05:43:17 -0400

    Update head.php

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -4,6 +4,10 @@
 
 <!-- start: CSS -->
 <link href="<?= $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/flatelements.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/jquery.mCustomScrollbar.min.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
 <!-- end: CSS -->
 
 <!-- start: JS file imports -->
