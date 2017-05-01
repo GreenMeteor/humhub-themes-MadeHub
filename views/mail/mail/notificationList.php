8778d89af68f3b61869d95c4bdfde6dd6d625795
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-05-01 03:35:56 -0400

    Update notificationList.php

--- views/mail/mail/notificationList.php
+++ views/mail/mail/notificationList.php
@@ -3,9 +3,6 @@
     <?php foreach ($userMessages as $userMessage) : ?>
         <?php echo $this->render('_messagePreview', array('userMessage' => $userMessage)); ?>
     <?php endforeach; ?>
-<?php else {
-    : ?>
-    <li class="placeholder"> <?php echo Yii::t('MailModule.views_mail_list', 'There are no messages yet.');
-}
-?></li>
+<?php else: ?>
+ -    <li class="placeholder"> <?php echo Yii::t('MailModule.views_mail_list', 'There are no messages yet.'); ?></li>
 <?php endif; ?>
